e58be8ff6e8ae6796a73c35c2335378b82a67ca4
pommee <hugoh@hotmail.se>
2025-03-31 20:40:10 +0200

    fix: remove unused dev plugin

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,11 +1,10 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, type PluginOption } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer() as PluginOption],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
