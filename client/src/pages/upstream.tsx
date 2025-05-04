706ba08ce1938dd6a6281a9f78e3af9372e1265c
pommee <hugoh@hotmail.se>
2025-05-04 11:01:24 +0200

    ui: spinner when loading upstreams

--- client/src/pages/upstream.tsx
+++ client/src/pages/upstream.tsx
@@ -1,6 +1,7 @@
 import { AddUpstream } from "@/app/upstream/addUpstream";
 import { UpstreamCard } from "@/app/upstream/card";
 import { GetRequest } from "@/util";
+import { ArrowClockwise } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
@@ -43,18 +44,27 @@ export function Upstream() {
       <div className="flex gap-5">
         <AddUpstream />
       </div>
-      <div className="grid lg:grid-cols-4 gap-2">
-        {upstreams?.upstreams.map((upstream, index) => (
-          <UpstreamCard
-            key={index}
-            dnsPing={upstream.dnsPing}
-            icmpPing={upstream.icmpPing}
-            name={upstream.name}
-            preferred={upstream.preferred}
-            upstream={upstream.upstream}
-          />
-        ))}
-      </div>
+      {(upstreams && (
+        <div className="grid lg:grid-cols-4 gap-2">
+          {upstreams?.upstreams.map((upstream, index) => (
+            <UpstreamCard
+              key={index}
+              dnsPing={upstream.dnsPing}
+              icmpPing={upstream.icmpPing}
+              name={upstream.name}
+              preferred={upstream.preferred}
+              upstream={upstream.upstream}
+            />
+          ))}
+        </div>
+      )) || (
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col items-center space-y-4">
+            <ArrowClockwise className="w-12 h-12 text-blue-400 animate-spin" />
+            <p className="text-lg text-stone-400">Loading upstreams...</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
