7914856c8d9de66efd8e76a0c1a67022a7aa6db9
Jørgen Teunis <jorgen@solvebright.com>
2025-06-10 20:33:03 +0200

    fix: increase arp lookup time since this can take longer on a bigger network

--- backend/dns/arp.go
+++ backend/dns/arp.go
@@ -45,7 +45,7 @@ func ProcessARPTable() {
 }
 
 func updateARPTable() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 40*time.Second)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, "arp", "-a")
