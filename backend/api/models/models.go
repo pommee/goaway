01efacf615ca50f0b3ddc903f2ac3e551d059dce
pommee <hugoh@hotmail.se>
2025-09-23 17:18:29 +0200

    chore: update query parameters for logs table

--- backend/api/models/models.go
+++ backend/api/models/models.go
@@ -1,10 +1,11 @@
 package models
 
 type QueryParams struct {
-	Page      int
-	PageSize  int
-	Offset    int
-	Search    string
-	Column    string
-	Direction string
+	Page         int
+	PageSize     int
+	Offset       int
+	Search       string
+	Column       string
+	Direction    string
+	FilterClient string
 }
