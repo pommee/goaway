4025b8bfe954d291ee2bd236c2b9fbcf69c155ad
pommee <hugoh@hotmail.se>
2025-09-28 21:04:30 +0200

    style: update to destructive button variant and fix pointer cursor case

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         default:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
+          "bg-destructive/50 text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
           "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
         secondary:
@@ -46,7 +46,9 @@ function Button({
   }) {
   const Comp = asChild ? Slot : "button";
   const cursorPointer =
-    props.disabled === undefined || false ? "cursor-pointer" : "";
+    props.disabled === undefined || props.disabled === false
+      ? "cursor-pointer"
+      : "";
 
   return (
     <Comp
