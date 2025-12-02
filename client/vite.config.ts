f17922401692165c1ccead40d479da8482f317df
pommee <hugoh@hotmail.se>
2025-12-02 18:31:14 +0100

    chore: fix build warning for client and better chunking
    Thanks to @authrequest for pointing this out in the PR https://github.com/pommee/goaway/pull/98

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -25,20 +25,18 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react-router")
-            ) {
+            if (id.includes("react") || id.includes("react-dom"))
               return "react-vendor";
-            }
+            if (id.includes("motion")) return "motion";
+            if (
+              id.includes("lucide-react") ||
+              id.includes("@phosphor-icons/react")
+            )
+              return "icons";
             return "vendor";
           }
-
-          if (id.includes("/src/components/") || id.includes("/src/pages/")) {
-            return "app";
-          }
-
+          if (id.includes("/src/pages/")) return "pages";
+          if (id.includes("/src/components/")) return "components";
           return null;
         }
       }
